ee4c38e78d4ccb8a61375f90c44e393886da96d0
leonardo <sponzal4@gmail.com>
2021-06-18 15:55:57 +0200

    snack

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const array = [];
+const numerirandom = 10;
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min;
 
+while (array.length < numerirandom){
+    array.push(randomNum(1,100));
+}
+
+console.log(array);
+
+const min = 1;
+const max = 10;
+
+let filter = [];
+
+array.forEach((element,index) => {
+    if(index >= min && index <= max){
+        filter.push(element);
+    }
+});
+console.log(filter);
